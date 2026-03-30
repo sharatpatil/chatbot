d021167d1e94836e6ef7f6d3fa287659b84f1fa4
sharatpatil <42792567+sharatpatil@users.noreply.github.com>
2026-03-30 21:54:11 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './Chatbot';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Chatbot />
+    </div>
+  );
+};
+
+export default App;
